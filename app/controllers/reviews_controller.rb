66cd93f6f99af25045dd2f24cf4a4ba150f06255
Thomas Birien <thomasbirien@gmail.com>
2017-02-15 18:00:30 +0100

    restaurant CRUD done et review(new and create) done, design not finished

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :find_restaurant_id
   def index
   end
 
@@ -6,9 +7,15 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @review = Review.new
   end
 
   def create
+    @review =  Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to @restaurant
+
   end
 
   def edit
@@ -18,5 +25,14 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def find_restaurant_id
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
